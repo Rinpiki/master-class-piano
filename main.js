022367728abce0ec7d3e80800c2a769095a6037c
Marcos Pablo <rinpikibr@gmail.com>
2022-04-21 14:49:56 -0300

    refatorando codigo

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 //get all keys
 const keys = document.querySelectorAll('.key');
 console.log(keys);
-//play notes
+
+ //play notes
+function playNote(event) {
+    
+    // Keycode
+    let audiokeycode = getkeyCode(event);
+    // typed or pressed key
+
+    // if key exists
+ 
+    // play audio
+}
+
+function getkeyCode(event) {
+    let keyCode;
+    const isKeyboard = event.type === "keydown"
+    if(isKeyboard) {
+      keyCode = event.keyCode
+    } else {
+        keyCode = event.target.dataset.key
+    }
+}
 
 //click with mouse
-
+keys.forEach(function(key) {
+    key.addEventListener("click", playNote)
+})
 //keyboard type
-window.addEventListener("keydown", playNotes)
+window.addEventListener("keydown", playNote)
