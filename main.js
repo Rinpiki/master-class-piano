b9a5f61ca74af08f99461c9a9b83e35daefef904
Marcos Pablo <rinpikibr@gmail.com>
2022-04-21 13:14:14 -0300

    add function event

---
+++ main.js
@@ -0,0 +1,9 @@
+//get all keys
+const keys = document.querySelectorAll('.key');
+console.log(keys);
+//play notes
+
+//click with mouse
+
+//keyboard type
+window.addEventListener("keydown", playNotes)
